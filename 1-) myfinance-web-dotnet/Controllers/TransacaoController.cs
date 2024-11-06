b0e56b63db530f67d853c3a16901875fecfb7db9
Carlos Henrique de Souza <carloshenriquesouza.eng@gmail.com>
2024-11-06 17:15:44 -0300

    feat: Tela de registro de transações e suas funcionalidades.

--- 1-) myfinance-web-dotnet/Controllers/TransacaoController.cs
+++ 1-) myfinance-web-dotnet/Controllers/TransacaoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using myfinance_web_dotnet.Models;
 using myfinance_web_dotnet_domain.Entities;
 using myfinance_web_dotnet_service.Interfaces;
@@ -10,14 +11,18 @@ namespace myfinance_web_dotnet.Controllers
     {
         private readonly ILogger<TransacaoController> _logger;
         private readonly ITransacaoService _transacaoService;
+        private readonly IPlanoContaService _planoContaService;
+
 
         public TransacaoController(
             ILogger<TransacaoController> logger,
-            ITransacaoService transacaoService
+            ITransacaoService transacaoService,
+            IPlanoContaService planoContaService
             )
         {
             _logger = logger;
             _transacaoService = transacaoService;
+            _planoContaService = planoContaService;
         }
 
         [HttpGet]
@@ -52,24 +57,27 @@ namespace myfinance_web_dotnet.Controllers
         [Route("Cadastrar/{Id}")]
         public IActionResult Cadastrar(int? Id)
         {
+            var ListaPlanoContas = new SelectList(_planoContaService.ListarRegistros(), "Id", "Descricao");
+            var itemTransacao = new TransacaoModel()
+            {
+                Data = DateTime.Now,
+                ListaPlanoContas = ListaPlanoContas
+            };
+
             if (Id != null)
             {
                 var transacao = _transacaoService.RetornarRegistro((int)Id);
-                var transacaoModel = new TransacaoModel()
-                {
-                    Id = transacao.Id,
-                    Historico = transacao.Historico,
-                    Data = transacao.Data,
-                    Valor = transacao.Valor,
-                    PlanoContaId = transacao.PlanoContaId,
-                };
 
-                return View(transacaoModel);
+                itemTransacao.Id = transacao.Id;
+                itemTransacao.Historico = transacao.Historico;
+                itemTransacao.Data = transacao.Data;
+                itemTransacao.Valor = transacao.Valor;
+                itemTransacao.PlanoContaId = transacao.PlanoContaId;
             }
-            else
-            {
-                return View();
-            }
+
+            return View(itemTransacao);
+
+
         }
 
         [HttpPost]
