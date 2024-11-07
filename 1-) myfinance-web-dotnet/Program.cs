b49f76416cad59c00695ba099c0574c44a35e918
Carlos Henrique de Souza <carloshenriquesouza.eng@gmail.com>
2024-11-07 11:22:16 -0300

    refactor: Variáveis de ambiente

--- 1-) myfinance-web-dotnet/Program.cs
+++ 1-) myfinance-web-dotnet/Program.cs
@@ -1,22 +1,29 @@
 using myfinance_web_dotnet_infra;
 using myfinance_web_dotnet_service;
 using myfinance_web_dotnet_service.Interfaces;
+using Microsoft.Extensions.Configuration; // Certifique-se de incluir isso
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adiciona serviços ao contêiner.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<MyFinanceDbContext>();
+
+// Configura a conexão do DbContext
+builder.Services.AddDbContext<MyFinanceDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+
+// Registra os serviços
 builder.Services.AddScoped<IPlanoContaService, PlanoContaService>();
 builder.Services.AddScoped<ITransacaoService, TransacaoService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configura o pipeline de requisições HTTP.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // O valor padrão do HSTS é 30 dias. Você pode querer alterar isso para cenários de produção.
     app.UseHsts();
 }
 
--- 4-) myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ 4-) myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using myfinance_web_dotnet_domain.Entities;
 
-namespace myfinance_web_dotnet_infra;
-
-public class MyFinanceDbContext: DbContext
+namespace myfinance_web_dotnet_infra
 {
-    public DbSet<PlanoConta> PlanoConta { get; set; }
-    public DbSet<Transacao> Transacao { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public class MyFinanceDbContext : DbContext
     {
-        optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=False;");
+        private readonly IConfiguration _configuration;
+        public DbSet<PlanoConta> PlanoConta { get; set; }
+        public DbSet<Transacao> Transacao { get; set; }
+        public MyFinanceDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _configuration.GetConnectionString("Database");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 }
